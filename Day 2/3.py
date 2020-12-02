82e56fc1b95d0600b42e73422d6a3c987e7475b8
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2020-12-02 20:20:43 +0100

    Cleaned up var names, used regex on most of part 1 and made it a lot more readable.

--- Day 2/3.py
+++ Day 2/3.py
@@ -2,47 +2,43 @@ import numpy as np
 import re
 
 array = []
-count = 0
-pwdcount = 0
-part2counter = 0
+pwd_count = 0
+first_final_count = 0
+second_final_count = 0
 
-filename = "input.txt"
-f = open(filename, "r")
+fi = open("input.txt", "r")
 
-for row in f:
+for row in fi:
     array.append(row)
 
 arr = np.array(array)
-
 for x in arr:
+    #Slicing the row (x) to make it clean.
     max = 0
     #Checking if there are 1 or 2 digits. If true 1 digit, false 2 digits.
     if "-" in x[0:2]:
         min = int(x[0])
         max = int(x[2:4])
-        count = x.count(x[5])
         #Checking if there are 1 or 2 digits in the max.
         if " " in x[2:4]:
             max = int(x[2])
-            count = x.count(x[4])
     else:
-        min = int(x[0:2])
         #Setting the max if there are 2 first digits.
+        min = int(x[0:2])
         max = int(x[3:5])
-        count = x.count(x[6])
-        
-    if min <= (count - 1) <= max:
-        pwdcount = pwdcount + 1
 
+    #Estabilishing regex stuff, then checking for the conditions as set in the exercise.
     regex = r"(: )"
-
     matches = re.search(regex, x)
-
-    if matches:
-        temp = x[matches.end():]
-        if (temp[min-1] == x[matches.end()-3]) ^ (temp[max-1] == x[matches.end()-3]):
-            part2counter = part2counter + 1
-
-
-print(f"Final pwd count: {pwdcount}")
-print(f"Part 2 counter: {part2counter}")
+    temp = x[matches.end():]
+    #First part
+    pwd_count = temp.count(x[matches.end()-3])
+    if min <= pwd_count <= max:
+        first_final_count = first_final_count + 1
+    #Second part
+    if (temp[min-1] == x[matches.end()-3]) ^ (temp[max-1] == x[matches.end()-3]):
+        second_final_count = second_final_count + 1
+    
+    
+print(f"Part 1 counter: {first_final_count}.")
+print(f"Part 2 counter: {second_final_count}.")
