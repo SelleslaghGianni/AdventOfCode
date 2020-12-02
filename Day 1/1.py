6dcc9c9a12a5a149a2f24869f38070afcacce447
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2020-12-02 20:36:30 +0100

    Melted 1 and 2 together

--- Day 1/1.py
+++ Day 1/1.py
@@ -5,11 +5,21 @@ f = open("input.txt", "r")
 for row in f:
     array.append(row)
 
-    
+first_part_bool = True
+second_part_bool = True
+first_part_answer = 0
+second_part_answer = 0
+
 for row in array:
     for i in array:
-        if (int(row) + int(i)) == 2020:
-            print(row)
-            print(i)
-            exit(0)
+        if (int(row) + int(i)) == 2020 and first_part_bool:
+            first_part_answer = int(row)*int(i)
+            first_part_bool = False
+        for j in array:
+            if (int(row) + int(i) + int(j)) == 2020 and second_part_bool:
+                second_part_answer = int(row)*int(i)*int(j)
+                second_part_bool = False
 
+
+print(f"First part: {first_part_answer}")
+print(f"Second part: {second_part_answer}")
